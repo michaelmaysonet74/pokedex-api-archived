e0b687e4579329983f9390eb1975972b59768d25
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2025-01-12 16:58:39 -0600

    Update PokemonRespone

--- src/main/java/dev/mmaysonet/pokedex/api/response/PokemonResponse.java
+++ src/main/java/dev/mmaysonet/pokedex/api/response/PokemonResponse.java
@@ -1,23 +1,22 @@
 package dev.mmaysonet.pokedex.api.response;
 
-import dev.mmaysonet.pokedex.entity.BaseStatsEntity;
-import dev.mmaysonet.pokedex.entity.MeasurementEntity;
-import dev.mmaysonet.pokedex.model.Ability;
-import java.util.List;
 import lombok.Builder;
 import lombok.NonNull;
+
+import java.util.List;
 
 @Builder(toBuilder = true)
 public record PokemonResponse(
     @NonNull Integer id,
     @NonNull String name,
-    List<Ability> abilities,
-    BaseStatsEntity baseStats,
+    List<AbilityResponse> abilities,
+    BaseStatsResponse baseStats,
     String category,
     String entry,
     EvolutionChainResponse evolution,
     Integer generation,
-    MeasurementEntity measurement,
+    MeasurementResponse measurement,
     String sprite,
     List<String> types,
-    List<String> weaknesses) {}
+    List<String> weaknesses) {
+}
